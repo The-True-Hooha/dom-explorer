3dd34123f4092b2c19e095543258634ccd7c9054
Davidhero <owogogahhero@outlook.com>
2024-09-16 18:01:53 +0100

    refactored: db file

--- app/database/database.py
+++ app/database/database.py
@@ -1,8 +1,7 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, Boolean, Column, ForeignKey, Integer, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, DateTime, func
-from sqlalchemy.orm import relationship
+from sqlalchemy.orm import sessionmaker, relationship
+from sqlalchemy.sql import func
 from app.core.core import app_setting
 
 SQLALCHEMY_DATABASE_URL = app_setting.DATABASE_URL
@@ -12,13 +11,14 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
 
+
 class User(Base):
     __tablename__ = "user"
-    
+
     id = Column(Integer, primary_key=True, index=True)
     email = Column(String, unique=True, index=True)
     password = Column(String)
-    domain_name = relationship("Domain", back_populates="user")
+    domains = relationship("Domain", back_populates="user")
     createdDate = Column(DateTime(timezone=True), server_default=func.now())
     
 
@@ -28,16 +28,19 @@ class Domain(Base):
     id = Column(Integer, primary_key=True, index=True)
     domain_name = Column(String, index=True)
     isActive = Column(Boolean, default=True)
-    sub_domain_name = relationship("SubDomain", back_populates="domain_name")
-    user = relationship("User", back_populates="domain_name")
+    sub_domains = relationship("SubDomain", back_populates="domain")
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship("User", back_populates="domains")
     createdDate = Column(DateTime(timezone=True), server_default=func.now())
     
     
 class SubDomain(Base):
     __tablename__ = "sub_domain"
+
     id = Column(Integer, primary_key=True, index=True)
     ctId = Column(String)
-    domain_name = relationship("Domain", back_populates="sub_domain_name")
+    domain_id = Column(Integer, ForeignKey('domain.id'))
+    domain = relationship("Domain", back_populates="sub_domains")
     issueName = Column(String)
     createdDate = Column(DateTime(timezone=True), server_default=func.now())
     
