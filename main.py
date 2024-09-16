455523c950f08fc47116a67c51fb6f023769c55e
Davidhero <owogogahhero@outlook.com>
2024-09-16 14:33:23 +0100

    provide structure

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, HTTPException, Query
+from fastapi import FastAPI, HTTPException, Query, status
 from fastapi.responses import HTMLResponse
 from pydantic import BaseModel
 from typing import List
@@ -7,3 +7,10 @@ from datetime import datetime
 
 app = FastAPI()
 
+
+@app.get("/health", status_code=status.HTTP_200_OK, description="get the health status of the database and sever")
+def run_health_check():
+    return {
+        "sever_status": "OK",
+        "database": "responsive"
+    }
