3cb87b66ac158ffb64bdfd626655dfae79256d72
Davidhero <owogogahhero@outlook.com>
2024-09-17 10:30:26 +0100

    refactor: update the schema file

--- app/schema/schema.py
+++ app/schema/schema.py
@@ -1,65 +1,69 @@
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, EmailStr, Field
 from typing import List, Optional
 from datetime import datetime
 
-class UserBase(BaseModel):
-    email: EmailStr
-    
-class UserCreate(UserBase):
-    password: str
-    
-class User(UserBase):
-    id: int
-    createdDate:datetime
-    
-    class Config:
-        orm_mode: True
 
-class DomainBase(BaseModel):
-    domain_name: str
-
-class DomainCreate(DomainBase):
-    pass
-
-class Domain(DomainBase):
-    id: int
-    isActive: bool
-    createdDate: datetime
-    user_id: int
-    
-    class config:
-        orm_mode = True
-    
 class SubDomainBase(BaseModel):
-    ctId: int
-    issueName: str
-    
+    name: str
+
 
 class SubDomainCreate(SubDomainBase):
     pass
 
 
-class SubDomain(SubDomainBase):
+class SubDomainResponse(SubDomainBase):
     id: int
     createdDate: datetime
-    domainId: int
-    
-    class config:
-        orm_mode: True
-    
+    domain_id: int
 
-class DomainWithSubDomains(Domain):
-    sub_domain: List[SubDomain]
-    
-    
-class UserWithDomain(User):
-    domains: List[Domain]
+    class ConfigDict:
+        from_attributes = True
+
+class DomainBase(BaseModel):
+    domain: str
+
+
+class DomainCreate(DomainBase):
+    isActive: bool = True
+
+
+class DomainResponse(DomainBase):
+    # id: int
+    # isActive: bool
+    # createdDate: datetime
+    # user_id: int
+    sub_domain: List[str] = []
+    count: int
+
+    class ConfigDict:
+        from_attributes = True
+
+
+class UserBase(BaseModel):
+    email: EmailStr
+
+
+class UserCreate(UserBase):
+    password: str
+
+
+class UserResponse(UserBase):
+    id: int
+    domains: List[DomainBase] = []
+    createdDate: datetime
+
+    class ConfigDict:
+        from_attributes = True
 
 
 class Token(BaseModel):
-    accessToken: str
-    tokenType: str
+    access_token: str = Field(..., alias="accessToken")
+    token_type: str = Field(..., alias="tokenType")
     exp: datetime
-    
+
+    class ConfigDict:
+        populate_by_name = True
+
+
 class TokenData(BaseModel):
     email: Optional[str] = None
